eb4f4510aae62355d8455e3f796fe7fedb5c4f3a
Cole Garstin <colegarstin@gmail.com>
2014-07-18 12:32:35 +0100

    built out basic css and sliding menu

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,7 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+$('.menu-toggle').on('click', function(){
+    if($(window).width() < 768){
+        $('nav ul').slideToggle().toggleClass('expanded');
+    }
+    return false;
+});
